38dc2ea7658915f728547705408e5d39d3197033
Thibault Garcia <97061165+ThibaultGarcia@users.noreply.github.com>
2023-12-10 13:21:47 +0100

    added removal button

--- exercice1_class.py
+++ exercice1_class.py
@@ -1,4 +1,4 @@
-from tkinter import (Tk, Canvas, N, W, E, S, Event, Button, Frame, RAISED, PhotoImage)
+from tkinter import (Tk, Canvas, N, W, E, S, Event, Button, Frame, PhotoImage)
 from PIL import Image, ImageTk
 from os import getcwd
 
@@ -13,6 +13,7 @@ class App(Tk):
         self.canva_bg = "gray"
         self.tk_sticky_all = (N,W,E,S)
         self.focused_tag=None
+        self.toggle_remove_flag=False
         self.frame = Frame(self)
         self.canva = Canvas(self.frame, height=self.height-100, width=self.width-120, bg=self.canva_bg)
 
@@ -21,20 +22,26 @@ class App(Tk):
         self.img_router = ImageTk.PhotoImage(Image.open("router.png"))
         self.img_switch = ImageTk.PhotoImage(Image.open("switch.png"))
 
-
-        self.button_pc = Button(self.frame,relief=RAISED,image=self.img_pc, command=lambda: self.spawn(self.img_pc))
-        self.button_router = Button(self.frame,relief=RAISED,image=self.img_router, command=lambda : self.spawn(self.img_router))
-        self.button_switch = Button(self.frame,relief=RAISED,image=self.img_switch, command=lambda : self.spawn(self.img_switch))
+        #définition des boutons
+        self.button_pc = Button(self.frame,image=self.img_pc, command=lambda: self.spawn(self.img_pc))
+        self.button_router = Button(self.frame,image=self.img_router, command=lambda: self.spawn(self.img_router))
+        self.button_switch = Button(self.frame,image=self.img_switch, command=lambda: self.spawn(self.img_switch))
 
         #définition de la grid
-        self.canva.grid(row=0, column=1,sticky=self.tk_sticky_all)
+        self.canva.grid(row=0, column=1,sticky=self.tk_sticky_all,columnspan=3)
         self.frame.grid(row=0, column=0,sticky=self.tk_sticky_all)
         
-        x,y = 1,0
+        #affichage des boutons par boucle for (f is for flemme)
+        x,y = 1,1
         for button in [self.button_pc, self.button_router, self.button_switch]:
             button.grid(row=x, column=y)
             y+=1
+        
+        self.img_remove = ImageTk.PhotoImage(Image.open("cross.png"))
+        self.button_remove = Button(self.frame,image=self.img_remove, command= lambda: self.toggle_remove() ).grid(row=0,column=0)
 
+
+    #apparition de l'élément cliqué
     def spawn(self,image):
         new_img = self.canva.create_image(350, 350, image=image)
         self.canva.tag_bind(new_img, "<Button-1>", lambda event : self.follow(event,True))
@@ -45,6 +52,8 @@ class App(Tk):
         y = event.y
         if is_clicked:
             self.focused_tag = self.canva.find_closest(x,y)[0]
+            if self.toggle_remove_flag:
+                self.canva.delete(self.focused_tag)
             self.canva.tag_raise(self.focused_tag)
         self.canva.moveto(self.focused_tag, x-24, y-24)
 
@@ -57,6 +66,14 @@ class App(Tk):
             self.mainloop()
         except KeyboardInterrupt:
             self.destroy()
+
+    def toggle_remove(self):
+        if self.toggle_remove_flag == False:
+            self.toggle_remove_flag = True
+            self.config(cursor="tcross")
+        else:
+            self.toggle_remove_flag = False
+            self.config(cursor="arrow")
 
 t = App("NDT - Network Drawing Thing",800,800)
 t.run()
